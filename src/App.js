19b723d9e8d441c3137fd1a24600f6dba4a821e1
Jon <schwamman@msn.com>
2019-10-25 22:39:54 -0500

    Removed rick astley url, added other audio files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Header from './components/header';
 import Soundboard from './components/soundboard';
 import audio from './data/audio';
